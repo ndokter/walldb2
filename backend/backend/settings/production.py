eb1e49cad4f887464be558b8c2372cae229f30fa
Nigel Dokter <mail@nldr.net>
2025-07-16 16:51:44 +0200

    production deploy fixes

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -3,6 +3,6 @@ from .base import *
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": config("REDIS_BACKEND"),
+        "LOCATION": os.environ.get("REDIS_BACKEND"),
     },
 }
