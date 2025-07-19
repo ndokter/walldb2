62c96ab38cf3632839954f50e35863ee9989ae40
Nigel Dokter <mail@nldr.net>
2025-07-19 15:00:00 +0200

    staticfiles fix

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -16,8 +16,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = '/app/mediafiles'
+
+STATIC_URL = 'static/'
+STATIC_ROOT = '/app/staticfiles'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -121,12 +124,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
