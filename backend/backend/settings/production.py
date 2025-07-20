f0ed5da2e33cfb2aae5a47d56ab58aa62f534b4d
Nigel Dokter <mail@nldr.net>
2025-07-20 14:57:40 +0200

    production deployment

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -1,5 +1,8 @@
 from .base import *
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
+
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
